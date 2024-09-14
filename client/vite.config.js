ab042c2568f4f9b4db637c40072c202a2cfa431c
Rahul Choudhary <rahulchoudhary0775@gmail.com>
2024-09-15 01:38:35 +0530

    updated vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
 
 
 // https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     host: '0.0.0.0', // Ensure it listens on all network interfaces
     port: 5173
-  }
+  },
+
+  plugins: [react()],
 });
+
