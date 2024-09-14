23625f1aa9740674e8b05039a2a424e74af5b8b0
Rahul Choudhary <rahulchoudhary0775@gmail.com>
2024-09-15 01:32:11 +0530

    vite chnages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Ensure it listens on all network interfaces
+    port: 5173
+  }
+});
